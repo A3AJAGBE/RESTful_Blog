1ab314e1afd38575e6ccaf9e1cc95761b0c515de
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-11 03:44:36 +0000

    Add database and posts table config

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 # Get the year
 current_year = datetime.now().year
 
 app = Flask(__name__)
+
+# Database Configs
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Posts Table
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'Post Title: {self.title}'
+
+
+db.create_all()
 
 
 @app.route('/')
