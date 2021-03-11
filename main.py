8044c3b1bae4e3baafa17159fc176e85a7377787
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-11 03:14:45 +0000

    Add year

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from datetime import datetime
+
+# Get the year
+current_year = datetime.now().year
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', year=current_year)
 
 
 if __name__ == '__main__':
