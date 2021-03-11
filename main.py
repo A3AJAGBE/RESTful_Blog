09430a1e6dfa8be665c5756681437d22403c5808
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-11 05:10:43 +0000

    Add form validation to new post function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from datetime import datetime
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor, CKEditorField
+from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -32,7 +32,7 @@ class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
@@ -60,9 +60,21 @@ def index():
     return render_template('index.html', year=current_year)
 
 
-@app.route('/new_post')
+@app.route('/new_post', methods=["GET", "POST"])
 def new_post():
     form = PostForm()
+    if form.validate_on_submit():
+        add_post = Posts(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+            body=form.body.data,
+            date=datetime.today().strftime("%B %d, %Y")
+        )
+        db.session.add(add_post)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('new_post.html', year=current_year, form=form)
 
 
