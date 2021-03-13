4abb5e3c94f9f0dd72689b403cb2cd42ea826e97
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-13 04:54:23 +0000

    Refactor tables and add admin only function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, abort
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import StringField, SubmitField, PasswordField, validators
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor, CKEditorField
-from flask_login import UserMixin, login_user, LoginManager, logout_user
+from flask_login import UserMixin, login_user, LoginManager, logout_user, current_user
+from functools import wraps
 from datetime import datetime
 import smtplib
 from dotenv import load_dotenv
@@ -43,27 +45,30 @@ def load_user(user_id):
     return Users.query.get(user_id)
 
 
+# Users Table
+class Users(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    posts = relationship("Posts", back_populates="author")
+
+
+# db.create_all()
+
+
 # Posts Table
 class Posts(db.Model):
+    __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
-
-    def __repr__(self):
-        return f'Post Title: {self.title}'
-
-
-# Users Table
-
-class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(500), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("Users", back_populates="posts")
 
 
 # db.create_all()
@@ -73,7 +78,6 @@ class Users(UserMixin, db.Model):
 class PostForm(FlaskForm):
     title = StringField("Post Title", [validators.InputRequired(message="The title field cannot be empty.")])
     subtitle = StringField("Subtitle", [validators.InputRequired(message="The subtitle field cannot be empty.")])
-    author = StringField("Author Name", [validators.InputRequired(message="The author field cannot be empty.")])
     img_url = StringField("Image URL", [validators.URL(message="That's not a valid url.")])
     # Add ckeditor editor field
     body = CKEditorField("Body", [validators.InputRequired(message="The body cannot be empty.")])
@@ -95,6 +99,18 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Enter")
 
 
+# admin-only decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If id is not 1 (administrator) then return 403 error
+        if current_user.id != 1:
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route('/')
 def index():
     all_posts = Posts.query.all()
@@ -108,15 +124,16 @@ def blog(blog_id):
 
 
 @app.route('/new_post', methods=["GET", "POST"])
+@admin_only
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
         add_post = Posts(
             title=form.title.data,
             subtitle=form.subtitle.data,
-            author=form.author.data,
             img_url=form.img_url.data,
             body=form.body.data,
+            author=current_user,
             date=datetime.today().strftime("%B %d, %Y")
         )
         db.session.add(add_post)
@@ -126,20 +143,19 @@ def new_post():
 
 
 @app.route('/edit/<int:blog_id>', methods=["GET", "POST"])
+@admin_only
 def edit(blog_id):
     post = Posts.query.get(blog_id)
     edit_post = PostForm(
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
         body=post.body
     )
     if edit_post.validate_on_submit():
         post.title = edit_post.title.data
         post.subtitle = edit_post.subtitle.data
         post.img_url = edit_post.img_url.data
-        post.author = edit_post.author.data
         post.body = edit_post.body.data
         db.session.commit()
         return redirect(url_for("blog", blog_id=post.id))
@@ -147,6 +163,7 @@ def edit(blog_id):
 
 
 @app.route("/delete/<int:blog_id>")
+@admin_only
 def delete(blog_id):
     delete_post = Posts.query.get(blog_id)
     db.session.delete(delete_post)
